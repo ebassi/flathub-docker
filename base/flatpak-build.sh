aed0b271f585ba41f48bff5f283f3104a9912743
Emmanuele Bassi <ebassi@gnome.org>
2017-04-04 11:00:54 +0100

    base: Move building Flatpak to a separate script
    This allows Docker to discard the files of the packages used to build
    Ostree and Flatpak from the layer, and reduces the size of the final
    image:
    
      Before:       ebassi/flathub  base     721.2 MB
      After:        ebassi/flathub  base     510.2 MB

---
+++ base/flatpak-build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+apt-get build-dep -qq -y --no-install-recommends \
+        ostree \
+        flatpak
+
+git clone --recursive https://github.com/ostreedev/ostree /root/ostree && \
+        cd /root/ostree && \
+        ./autogen.sh \
+                --prefix /usr \
+                --sysconfdir /etc \
+                --libdir /usr/lib/x86_64-linux-gnu \
+                --libexecdir /usr/bin \
+                --localstatedir /var \
+                --disable-silent-rules \
+                --disable-gtk-doc \
+                --disable-man \
+                --with-dracut \
+                --with-grub2 \
+                --with-grub2-mkconfig-path=/usr/sbin/grub-mkconfig \
+                --with-systemdsystemunitdir=/lib/systemd/system && \
+        make && make install && \
+        rm -f /usr/lib/*/*.la && \
+        cd /root && \
+        rm -rf /root/ostree
+
+git clone --recursive https://github.com/flatpak/flatpak /root/flatpak && \
+        cd /root/flatpak && \
+        ./autogen.sh \
+                --prefix /usr \
+                --sysconfdir /etc \
+                --libdir /usr/lib/x86_64-linux-gnu \
+                --libexecdir /usr/bin \
+                --localstatedir /var \
+                --disable-silent-rules \
+                --disable-docbook-docs \
+                --disable-gtk-doc \
+                --disable-installed-tests \
+                --disable-documentation \
+                --with-priv-mode=none \
+                --with-privileged-group=sudo \
+                --with-systemdsystemunitdir=/lib/systemd/system && \
+        make && make install && \
+        rm -f /usr/lib/*/*.la && \
+        cd /root && \
+        rm -rf /root/flatpak
+
+apt-get remove -y \
+        adwaita-icon-theme \
+        autotools-dev \
+        bison \
+        build-essential \
+        debhelper \
+        docbook \
+        docbook-to-man \
+        docbook-xml \
+        docbook-xsl \
+        fontconfig \
+        hicolor-icon-theme \
+        highlight \
+        highlight-common \
+        intltool-debian \
+        libcairo-gobject2 \
+        libcairo2 \
+        libepoxy0 \
+        libfontconfig1 \
+        libharfbuzz0b \
+        libgtk-3-0 \
+        libicu-dev \
+        man-db \
+        python \
+        python2.7 \
+        python2.7-minimal \
+        python3 \
+        python3.5 \
+        python3.5-minimal \
+        sgml-base \
+        sgml-data \
+        xml-core \
+        xmlto \
+        xorg-sgml-doctools \
+        xsltproc
+
+rm -rf /usr/share/doc/* /usr/share/man/*
